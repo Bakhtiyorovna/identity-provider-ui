6031378747b245635651b5a461dac4aab6b79267
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-09-07 11:41:58 +0500

    Add button style design

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -73,7 +73,7 @@ export default defineComponent({
         </div>
         <button
           type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="login-btn w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Login to your account
         </button>
@@ -115,6 +115,10 @@ export default defineComponent({
   width: full;
   display: flex;
   margin-right:30px;
+}
+
+.login-btn:hover{
+  box-shadow: 0 0 10px blue;
 }
 
 .asosiy-sci a {
